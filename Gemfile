2252488d8d765819dc7e4d44b1fb8bf10e28127f
rnd_dev <rnd@localhost.localdomain>
2015-02-05 06:36:32 +0900

    Sinatra導入して下地書いた

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+gem 'sinatra'
 gem 'tweetstream'
 gem 'foreman'
 gem 'nokogiri', '>= 1.6'
--- main.rb
+++ main.rb
@@ -1,5 +1,38 @@
-require './bot'
+require 'sinatra'
+#require './bot'
+require './lib/nomlish_api'
+begin
+  STREAMING = Thread.new do
+    Thread.handle_interrupt(RuntimeError=>:on_blocking) do
+#      Bot.init 'BotConfig.yaml'
+      if Thread.pending_interrupt?
+        sleep 1
+        #TODO TwitterBot processe will be written here
+      end
+    end
+  end
+  STREAMING.join
+rescue
+  #TODO TwitterBot send error
+end
+#ConsoleScreenProcess
+class Console < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-Bot.init 'BotConfig.yaml'
-Bot.run
+  get '/edit' do
+    erb :edit
+  end
 
+  post '/stop' do
+    STREAMING.raise
+    redirect back
+  end
+
+  post '/run' do
+    STREAMING.run
+    redirect back
+  end
+end
+Console.run!
